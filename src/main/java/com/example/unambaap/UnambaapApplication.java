e5d2ff955d5555e12a0ab43504db24a5365c0843
00Flaptzy <222151@unamba.edu.pe>
2024-11-06 11:45:59 -0500

    primer commit - proyecto inicial

---
+++ src/main/java/com/example/unambaap/UnambaapApplication.java
@@ -0,0 +1,13 @@
+package com.example.unambaap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnambaapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnambaapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/unambaap/UnambaapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unambaap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnambaapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
